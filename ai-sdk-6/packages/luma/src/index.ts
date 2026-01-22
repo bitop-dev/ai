cdb01ca67e710f20657176ed3b44dfb65ecf4177
Nicholas Cecere <nick@bitop.dev>
2026-01-22 08:08:28 -0500

    refactor: clean up of tasks and refrences

--- ai-sdk-6/packages/luma/src/index.ts
+++
@@ -1,7 +0,0 @@
-export { createLuma, luma } from './luma-provider';
-export type { LumaProvider, LumaProviderSettings } from './luma-provider';
-export type {
-  LumaErrorData,
-  LumaImageProviderOptions,
-} from './luma-image-model';
-export { VERSION } from './version';
